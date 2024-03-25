90a261904a3eb9ebf2e0ee570eda790d07aa630a
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-25 14:56:15 +0100

    ADD: bonus img link

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello World of VUE!'
+        message: 'Hello World of VUE!',
+        img: 'https://i.ytimg.com/vi/qvFG8J7SUDE/maxresdefault.jpg'
       }
     }
   }).mount('#title')
